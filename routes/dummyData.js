d4f8aa45d27d881d7df4fb28fb01200e877373f5
Utkarsh Singh <utkarsh.singh.19@mountblue.tech>
2022-03-31 12:00:45 +0530

    dummy dataset, create and retrieve methods added

---
+++ routes/dummyData.js
@@ -0,0 +1,3 @@
+const dummyUsers = [{ "_id": "fccfb658-e088-432d-8cb1-5f8083ea4250", "name": "Utkarsh Singh", "age": 21 }, { "_id": "15815f69-3999-4ae1-959c-5d5665313b4c", "name": "Pawan Kalyan", "age": 21 }, { "_id": "d808fb71-7c53-4b31-aa3b-3bbd25452bf0", "name": "Chinmaya Patel", "age": 27 }]
+
+export default dummyUsers
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,32 @@
 // imports
 import express from "express"
-
+import { v4 as uuidv4 } from 'uuid';
+import dummyUsers from "./dummyData.js"
 // router init
 const router = express.Router()
 
 
-// routes defined for users
-router.get("/", (req, res) => {
-    res.send("Hello from userRoutes")
+// custom routes defined for users
+//  * create one user
+router.post("/", (req, res) => {
+    let newUser = req.body;
+    dummyUsers.push(newUser)
+    res.send("a new user added")
 })
 
+// * retrive all users
+router.get("/", (req, res) => {
+    res.send(dummyUsers);
+})
+
+// * retrieve single user
+router.get("/:id", (req, res) => {
+    const userID = req.params.id
+    const userFound = dummyUsers.find((user) => user._id == userID)
+    res.send(userFound)
+})
+
+// console.log(uuidv4())
 
 // export router
 export default router;
